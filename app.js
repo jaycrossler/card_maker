8b68693718dfcb7bc2f705e24343112fad849f2e
Jay Crossler <jay.crossler@gmail.com>
2016-10-08 10:34:35 -0400

    Split into scripts library structure

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 var express = require('express')
     , request = require('request')
-//, _ = require('underscore')
-//    , Canvas = require('canvas')
     , jsonfile = require('jsonfile')
     , fs = require('fs')
+    , card_drawing = require('./scripts/card_drawing')
     , fabric = require('fabric').fabric;
 
 var app = express();
-var card_width = 825,
-    card_height = 1125,
-    card_count = 81, //81,
-    thumbnail_scale = 0.2,
-    border_1 = 30, border_1_round = 20,
-    sheet_url = "https://spreadsheets.google.com/feeds/list/1r2bJjGhoaIfm8iEfsCHKiu3oSOznx5H2HFhwnWwYfjs/od6/public/values?alt=json",
+var sheet_url = "https://spreadsheets.google.com/feeds/list/1r2bJjGhoaIfm8iEfsCHKiu3oSOznx5H2HFhwnWwYfjs/od6/public/values?alt=json",
     card_data = null,
     bg_image = null,
     bg_src = 'images/blue_hex_bg.png',
@@ -56,32 +50,6 @@ function init(){
 }
 
 
-function show_thumbnails() {
-
-    var image_w = card_width * thumbnail_scale;
-    var image_h = card_height * thumbnail_scale;
-    var html = '';
-
-    html += '<html><head><title>Limits Of Virtue Cards</title>';
-    html += '<style>.card {border:1px solid lightblue; margin:4px;width:'+image_w+'px;height:'+image_h+'px;}</style>';
-    html += '</head><body>';
-    html += '<h1>Limits Of Virtue Cards</h1>';
-    for (var card_id=0; card_id<card_count; card_id++) {
-        html += '<a href="/card/' + card_id + '"><img src="/card/' + card_id + '" class="card"/></a>';
-    }
-
-    html += '<br/><a href="/flush">Reload card data from Google Sheets</a>';
-    html += '</body></html>';
-
-    return html;
-}
-
-//Temporary Randomness function
-var rand_seed = 1;
-function random() {
-    var x = Math.sin(rand_seed++) * 10000;
-    return x - Math.floor(x);
-}
 
 
 //=================================================
@@ -92,9 +60,32 @@ app.get('/', function (req, res) {
     //TODO: Have a way to show alternate titles
     //TODO: Pull real keywords instead of random ones
 
-    res.write(show_thumbnails());
+    var h = "<html><head><title>The Size of Your Deck!</title></head><body>";
+    h += "<h1>The Size of your Deck!</h1>";
+    h += "<li><a href='/cards/images/big'>All cards as images (full sized)</a></li>";
+    h += "<li><a href='/cards/images/small'>All cards as images (small)</a></li>";
+    h += "<li><a href='/cards/pdf/big'>All cards as PDF sheet (small)</a></li>";
+    h += "<li><a href='/cards/pdf/small'>All cards as PDF sheet (small)</a></li>";
+    h += "<li><a href='/flush'>Reload card data from Google Sheets</a></li>";
+    h += "<br/>Here are a few cards:<br/>";
+    h += card_drawing.show_thumbnails({size:'small',all:false});
+    h += "</body></html>";
+
+    res.write(h);
     res.end();
 });
+
+
+app.get('/cards/images/big', function (req, res) {
+    res.write(card_drawing.show_thumbnails({size:'big',all:true}));
+    res.end();
+});
+
+app.get('/cards/images/small', function (req, res) {
+    res.write(card_drawing.show_thumbnails({size:'small',all:true}));
+    res.end();
+});
+
 
 //Re-load all data
 app.get('/flush', function (req, res) {
@@ -115,6 +106,7 @@ app.get('/card/:cardId', function (req, res) {
     var dice_text = this_card_data['gsx$dice']['$t']; //TODO: Build some exception handling
     var num_text = this_card_data['gsx$result']['$t'];
     var title_text = this_card_data['gsx$text']['$t'];
+    var story_text = "1LT Dewberry was a helicopter pilot - well liked, intelligent and popular with the ladies.  Unfortunately, none of those traits discouraged the incomming MANPAD rocket.";
 
     rand_seed = card_id;
     var items = 'Artillery Scouts Stealth Airpower Cyber Logistics Leader Weather Terrain Tunnel Sabotage Charge Fuel Morale'.split(' ');
@@ -135,210 +127,26 @@ app.get('/card/:cardId', function (req, res) {
         }
     }
 
-
-    //Create the canvas
-    var canvas_fabric = fabric.createCanvasForNode(card_width, card_height);
-    canvas_fabric.backgroundColor = 'black';
-    canvas_fabric.setBackgroundImage(bg_image);
-
-
-
-    //Build "border_1" rectangle around the border
-    var rect = new fabric.Rect({
-        width: card_width - (border_1 * 2)
-        , height: card_height - (border_1 * 2)
-        , rx: border_1_round
-        , ry: border_1_round
-        , left: border_1
-        , top: border_1
-        , fill: 'rgba(0, 0, 0, 0)'
-        , strokeWidth: 4
-        , stroke: 'yellow' });
-    canvas_fabric.add(rect);
-
-    var line_vert_pos = border_1 + 160;
-    var underline = new fabric.Line([border_1, line_vert_pos, card_width - border_1, line_vert_pos],{
-        strokeWidth: 4
-        , stroke: 'yellow' });
-    canvas_fabric.add(underline);
-
-    var text = new fabric.Text(title_text, {
-        left: 100
-        , top: 70
-        , fill: 'yellow'
-        , textAlign: 'center'
-        , fontSize: (title_text.length > 20 ? 68 : 80)
-        , fontWeight: 'bold'
-        , fontFamily: 'Impact'
-    });
-    canvas_fabric.add(text);
-    canvas_fabric.centerObjectH(text);
-
-
-    //TODO: Make these a function and have pos/fontsize differ by content
-    var height_mod = -30;
-    var diamond_1 = new fabric.Rect({
-        left: card_width /2,
-        top: (card_height /2) - 205 + height_mod,
-        fill: 'yellow',
-        width: 200,
-        height: 200,
-        angle:45,
-        opacity: 0.9 });
-    canvas_fabric.add(diamond_1);
-    var text_1 = new fabric.Text(dice_text[0], {
-        left: (card_width /2) - 52 + (dice_text[0] == "-" ? 24 : 0)
-        , top: (card_height /2) - 160 + height_mod
-        , fill: (dice_text[0] == "-") ? "red" : "black"
-        , textAlign: 'center'
-        , fontSize: 200
-        , fontWeight: 'bold'
-        , fontFamily: 'Impact'
-    });
-    canvas_fabric.add(text_1);
-    var callout_1 = new fabric.Text(keyword_1, {
-        left: (card_width /2) - 180
-        , top: (card_height /2) - 90 + height_mod
-        , originX : 'center', originY: 'center'
-        , angle: -45
-        , fill: "white"
-        , fontSize: 80
-        , fontWeight: 'bold'
-        , fontFamily: 'Impact'
-    });
-    canvas_fabric.add(callout_1);
-
-
-
-
-    var diamond_2 = new fabric.Rect({
-        left: (card_width /2) - 145,
-        top: (card_height /2) - 60 + height_mod,
-        fill: 'yellow',
-        width: 200,
-        height: 200,
-        angle:45,
-        opacity: 0.9 });
-    canvas_fabric.add(diamond_2);
-    var text_2 = new fabric.Text(dice_text[1], {
-        left: (card_width /2) - 196 + (dice_text[1] == "-" ? 24 : 0)
-        , top: (card_height /2) - 19 + height_mod
-        , fill: (dice_text[1] == "-") ? "red" : "black"
-        , textAlign: 'center'
-        , fontSize: 200
-        , fontWeight: 'bold'
-        , fontFamily: 'Impact'
-    });
-    canvas_fabric.add(text_2);
-    var callout_2 = new fabric.Text(keyword_2, {
-        left: (card_width /2) + 180
-        , top: (card_height /2) - 90 + height_mod
-        , angle: 45
-        , originX : 'center', originY: 'center'
-        , fill: "white"
-        , fontSize: 80
-        , fontWeight: 'bold'
-        , fontFamily: 'Impact'
-    });
-    canvas_fabric.add(callout_2);
-
-
-    var diamond_3 = new fabric.Rect({
-        left: (card_width /2) + 145,
-        top: (card_height /2) - 60 + height_mod,
-        fill: 'yellow',
-        width: 200,
-        height: 200,
-        angle:45,
-        opacity: 0.9 });
-    canvas_fabric.add(diamond_3);
-    var text_3 = new fabric.Text(dice_text[2], {
-        left: (card_width /2) + 94 + (dice_text[2] == "-" ? 24 : 0)
-        , top: (card_height /2) - 19 + height_mod
-        , fill: (dice_text[2] == "-") ? "red" : "black"
-        , textAlign: 'center'
-        , fontSize: 200
-        , fontWeight: 'bold'
-        , fontFamily: 'Impact'
-    });
-    canvas_fabric.add(text_3);
-    var callout_3 = new fabric.Text(keyword_3, {
-        left: (card_width /2) + 170
-        , top: (card_height /2) + 260 + height_mod
-        , angle: 135
-        , originX : 'center', originY: 'center'
-        , fill: "white"
-        , fontSize: 80
-        , fontWeight: 'bold'
-        , fontFamily: 'Impact'
-    });
-    canvas_fabric.add(callout_3);
-
-    var diamond_4 = new fabric.Rect({
-        left: card_width /2,
-        top: (card_height /2) + 85 + height_mod,
-        fill: 'yellow',
-        width: 200,
-        height: 200,
-        angle:45,
-        opacity: 0.9 });
-    canvas_fabric.add(diamond_4);
-    var text_4 = new fabric.Text(dice_text[3], {
-        left: (card_width /2) - 52 + (dice_text[3] == "-" ? 24 : 0)
-        , top: (card_height /2) + 130 + height_mod
-        , fill: (dice_text[3] == "-") ? "red" : "black"
-        , textAlign: 'center'
-        , fontSize: 200
-        , fontWeight: 'bold'
-        , fontFamily: 'Impact'
-    });
-    canvas_fabric.add(text_4);
-    var callout_4 = new fabric.Text(keyword_4, {
-        left: (card_width /2) - 170
-        , top: (card_height /2) + 260 + height_mod
-        , angle: 225
-        , originX : 'center', originY: 'center'
-        , fill: "white"
-        , fontSize: 80
-        , fontWeight: 'bold'
-        , fontFamily: 'Impact'
-    });
-    canvas_fabric.add(callout_4);
-
-    //Small tile of total score in bottom right
-    var tile_width = 220;
-    var rect_small = new fabric.Rect({
-        width: tile_width
-        , height: tile_width
-        , left: card_width - tile_width - border_1
-        , top: card_height - tile_width - border_1
-        , rx: border_1_round
-        , ry: border_1_round
-        , strokeWidth: 4
-        , stroke: 'yellow' });
-    canvas_fabric.add(rect_small);
-
-    if (num_text >= 0) num_text = "+" + num_text;
-    var text_total = new fabric.Text(num_text, {
-        left: card_width - border_1 - (tile_width * .8) - (num_text < 0 ? 0 : 25)
-        , top: card_height - border_1 - (tile_width * .88)
-        , fill: (num_text < 0) ? "red" : "yellow"
-        , textAlign: 'center'
-        , fontSize: (tile_width * .8)
-        , fontWeight: 'bold'
-        , fontFamily: 'Impact'
-    });
-    canvas_fabric.add(text_total);
-
+    var options = {
+        design_style : 'black_hex',
+        bg_image : bg_image,
+        card_id: card_id,
+        this_card_data: this_card_data,
+        dice_text : dice_text,
+        num_text : num_text,
+        title_text : title_text,
+        story_text : story_text,
+        keywords : [keyword_1, keyword_2, keyword_3, keyword_4]
+    };
 
     //Export as PNG to browser
-    var stream = canvas_fabric.createPNGStream();
+    var stream = card_drawing.draw_card(options);
     res.setHeader('Content-Type', 'image/png');
     stream.pipe(res);
 
 
     //Save PNG to directory of cards
-    var path = __dirname + '/images/cards/card_' + card_id + '.png';
+    var path = __dirname + '/images/cards/card_' + card_id + '.png'; //TODO: save based on build
     var out = fs.createWriteStream(path);
     stream.on('data', function(chunk) {
         out.write(chunk);
@@ -349,3 +157,14 @@ app.get('/card/:cardId', function (req, res) {
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+
+//--------------------------------
+//Utility Functions
+
+//Temporary Randomness function
+var rand_seed = 1;
+function random() {
+    var x = Math.sin(rand_seed++) * 10000;
+    return x - Math.floor(x);
+}
---
+++ scripts/card_drawing.js
@@ -0,0 +1,253 @@
+var fabric = require('fabric').fabric;
+
+
+module.exports = {
+    show_thumbnails : show_thumbnails,
+    draw_card : draw_card,
+    test : function(){return "test"}
+};
+
+var card_width = 825,
+    card_height = 1125,
+    card_count = 81, //81,
+    border_1 = 30, border_1_round = 20,
+    thumbnail_scale = 0.2;
+
+
+
+
+
+function show_thumbnails(options) {
+
+    //If options.all, show all cards, otherwise only every 9th
+    var skip_count = (options && options.all) ? 1 : 9;
+
+    var image_w = card_width * thumbnail_scale;
+    var image_h = card_height * thumbnail_scale;
+    var html = '';
+
+    html += '<style>.card {border:1px solid lightblue; margin:4px;width:'+image_w+'px;height:'+image_h+'px;}</style>';
+    for (var card_id=0; card_id<card_count; card_id+=skip_count) {
+        html += '<a href="/card/' + card_id + '"><img src="/card/' + card_id + '" class="card"/></a>';
+    }
+
+    return html;
+}
+
+
+function draw_card(options) {
+
+    //Create the canvas
+    var canvas_fabric = fabric.createCanvasForNode(card_width, card_height);
+    canvas_fabric.backgroundColor = 'black';
+    canvas_fabric.setBackgroundImage(options.bg_image);
+
+
+
+    //Build "border_1" rectangle around the border
+    var rect = new fabric.Rect({
+        width: card_width - (border_1 * 2)
+        , height: card_height - (border_1 * 2)
+        , rx: border_1_round
+        , ry: border_1_round
+        , left: border_1
+        , top: border_1
+        , fill: 'rgba(0, 0, 0, 0)'
+        , strokeWidth: 4
+        , stroke: 'yellow' });
+    canvas_fabric.add(rect);
+
+    var line_vert_pos = border_1 + 160;
+    var underline = new fabric.Line([border_1, line_vert_pos, card_width - border_1, line_vert_pos],{
+        strokeWidth: 4
+        , stroke: 'yellow' });
+    canvas_fabric.add(underline);
+
+    var text = new fabric.Text(options.title_text, {
+        left: 100
+        , top: 70
+        , fill: 'yellow'
+        , textAlign: 'center'
+        , fontSize: (options.title_text.length > 20 ? 68 : 80)
+        , fontWeight: 'bold'
+        , fontFamily: 'Impact'
+    });
+    canvas_fabric.add(text);
+    canvas_fabric.centerObjectH(text);
+
+
+    //TODO: Make these a function and have pos/fontsize differ by content
+    var height_mod = -35;
+    var diamond_1 = new fabric.Rect({
+        left: card_width /2,
+        top: (card_height /2) - 205 + height_mod,
+        fill: 'yellow',
+        width: 200,
+        height: 200,
+        angle:45,
+        opacity: 0.9 });
+    canvas_fabric.add(diamond_1);
+    var text_1 = new fabric.Text(options.dice_text[0], {
+        left: (card_width /2) - 52 + (options.dice_text[0] == "-" ? 24 : 0)
+        , top: (card_height /2) - 160 + height_mod
+        , fill: (options.dice_text[0] == "-") ? "red" : "black"
+        , textAlign: 'center'
+        , fontSize: 200
+        , fontWeight: 'bold'
+        , fontFamily: 'Impact'
+    });
+    canvas_fabric.add(text_1);
+    var callout_1 = new fabric.Text(options.keywords[0], {
+        left: (card_width /2) - 180
+        , top: (card_height /2) - 90 + height_mod
+        , originX : 'center', originY: 'center'
+        , angle: -45
+        , fill: "white"
+        , fontSize: 80
+        , fontWeight: 'bold'
+        , fontFamily: 'Impact'
+    });
+    canvas_fabric.add(callout_1);
+
+
+
+
+    var diamond_2 = new fabric.Rect({
+        left: (card_width /2) - 145,
+        top: (card_height /2) - 60 + height_mod,
+        fill: 'yellow',
+        width: 200,
+        height: 200,
+        angle:45,
+        opacity: 0.9 });
+    canvas_fabric.add(diamond_2);
+    var text_2 = new fabric.Text(options.dice_text[1], {
+        left: (card_width /2) - 196 + (options.dice_text[1] == "-" ? 24 : 0)
+        , top: (card_height /2) - 19 + height_mod
+        , fill: (options.dice_text[1] == "-") ? "red" : "black"
+        , textAlign: 'center'
+        , fontSize: 200
+        , fontWeight: 'bold'
+        , fontFamily: 'Impact'
+    });
+    canvas_fabric.add(text_2);
+    var callout_2 = new fabric.Text(options.keywords[1], {
+        left: (card_width /2) + 180
+        , top: (card_height /2) - 90 + height_mod
+        , angle: 45
+        , originX : 'center', originY: 'center'
+        , fill: "white"
+        , fontSize: 80
+        , fontWeight: 'bold'
+        , fontFamily: 'Impact'
+    });
+    canvas_fabric.add(callout_2);
+
+
+    var diamond_3 = new fabric.Rect({
+        left: (card_width /2) + 145,
+        top: (card_height /2) - 60 + height_mod,
+        fill: 'yellow',
+        width: 200,
+        height: 200,
+        angle:45,
+        opacity: 0.9 });
+    canvas_fabric.add(diamond_3);
+    var text_3 = new fabric.Text(options.dice_text[2], {
+        left: (card_width /2) + 94 + (options.dice_text[2] == "-" ? 24 : 0)
+        , top: (card_height /2) - 19 + height_mod
+        , fill: (options.dice_text[2] == "-") ? "red" : "black"
+        , textAlign: 'center'
+        , fontSize: 200
+        , fontWeight: 'bold'
+        , fontFamily: 'Impact'
+    });
+    canvas_fabric.add(text_3);
+    var callout_3 = new fabric.Text(options.keywords[2], {
+        left: (card_width /2) + 170
+        , top: (card_height /2) + 260 + height_mod
+        , angle: 135
+        , originX : 'center', originY: 'center'
+        , fill: "white"
+        , fontSize: 80
+        , fontWeight: 'bold'
+        , fontFamily: 'Impact'
+    });
+    canvas_fabric.add(callout_3);
+
+    var diamond_4 = new fabric.Rect({
+        left: card_width /2,
+        top: (card_height /2) + 85 + height_mod,
+        fill: 'yellow',
+        width: 200,
+        height: 200,
+        angle:45,
+        opacity: 0.9 });
+    canvas_fabric.add(diamond_4);
+    var text_4 = new fabric.Text(options.dice_text[3], {
+        left: (card_width /2) - 52 + (options.dice_text[3] == "-" ? 24 : 0)
+        , top: (card_height /2) + 130 + height_mod
+        , fill: (options.dice_text[3] == "-") ? "red" : "black"
+        , textAlign: 'center'
+        , fontSize: 200
+        , fontWeight: 'bold'
+        , fontFamily: 'Impact'
+    });
+    canvas_fabric.add(text_4);
+    var callout_4 = new fabric.Text(options.keywords[3], {
+        left: (card_width /2) - 170
+        , top: (card_height /2) + 260 + height_mod
+        , angle: 225
+        , originX : 'center', originY: 'center'
+        , fill: "white"
+        , fontSize: 80
+        , fontWeight: 'bold'
+        , fontFamily: 'Impact'
+    });
+    canvas_fabric.add(callout_4);
+
+    //Small tile of total score in bottom right
+    var tile_width = 120;
+    var rect_small = new fabric.Rect({
+        width: tile_width
+        , height: tile_width
+        , left: ((card_width - tile_width) / 2)
+        , top: tile_width + border_1 + 20
+        , rx: border_1_round
+        , ry: border_1_round
+        , strokeWidth: 4
+        , stroke: 'yellow' });
+    canvas_fabric.add(rect_small);
+
+    if (options.num_text >= 0) options.num_text = "+" + options.num_text;
+    var text_total = new fabric.Text(options.num_text, {
+        left: ((card_width) / 2)
+        , top: (tile_width * .4) + border_1 + 162
+        , fill: (options.num_text < 0) ? "red" : "yellow"
+        , originX : 'center', originY: 'center'
+        , fontSize: (tile_width * .8)
+        , fontWeight: 'bold'
+        , fontFamily: 'Impact'
+    });
+    canvas_fabric.add(text_total);
+
+    var text_story = new fabric.Textbox(options.story_text, {
+        left: ((card_width) / 2)
+        , top: card_height - border_1 - 30
+        , originX : 'center', originY: 'bottom'
+        , fontSize: 28
+        , height: 200
+        , textAlign: 'center'
+        , width: card_width - (border_1 * 2) - 40
+        , fontWeight: 'bold'
+        //, stroke: 'black'
+        , fill: 'yellow'
+        , fontFamily: 'Impact'
+
+    });
+    canvas_fabric.add(text_story);
+
+
+    return canvas_fabric.createPNGStream();
+
+}
